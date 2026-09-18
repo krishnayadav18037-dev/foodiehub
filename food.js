33aebbb861c40eba5a050fa53a3d8368f2eac708
Krishna Yadav <krishnayadav18037@gmail.com>
2026-09-18 13:50:07 +0530

    Initial commit

---
+++ food.js
@@ -0,0 +1,24 @@
+let userdata = [
+    {
+    name: "bhola yadav",
+    age : 25,
+    class : "3rd year"
+    },
+    {
+        name : "rahul yadav",
+        age: 26,
+        class : "4th year"
+    }
+]
+for(let user of userdata){
+    console.log(user);
+}
+
+
+let fruits = ["apple", "banana", "mango", "grapes"];
+for(let fruit of fruits){
+    console.log(fruit);
+}
+
+
+let profile = ["ram", 24, "3rd year"];
